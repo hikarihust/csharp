702d7f8ae49387e08edb166ecc402f663ad551d1
vudinhquang <vudinhquangk53@gmail.com>
2021-08-06 00:14:52 +0900

    Hello world

--- csharp/csharp/Program.cs
+++ csharp/csharp/Program.cs
@@ -6,7 +6,7 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello C#");
         }
     }
 }
