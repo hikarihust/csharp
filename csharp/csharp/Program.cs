589a3af19c174223f6d6753c4d74e606e863f7a9
vudinhquang <vudinhquangk53@gmail.com>
2021-08-06 23:17:24 +0900

    Split

--- csharp/csharp/Program.cs
+++ csharp/csharp/Program.cs
@@ -7,7 +7,39 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            Check();
+            Console.OutputEncoding = Encoding.UTF8;
+            SplitArray();
+        }
+
+        static void SplitArray()
+        {
+            int[] arrInt = { 1, 2, 4, 8, 0, 4, 2, 3, 5, 1 };
+
+            // Input: 1 array
+            // Output: 2 array, chẵn 2, 4, 8, 0, 4, 2,  lẻ 1 3, 5, 1 
+            int length = arrInt.Length;
+            int indexArrEven = 0;
+            int indexArrOdd = 0;
+
+            int[] arrEven = new int[length];
+            int[] arrOdd = new int[length];  // dư
+
+            for (int i = 0; i < length; i++)
+            {
+                if (arrInt[i] % 2 == 0)
+                {
+                    arrEven[indexArrEven] = arrInt[i];
+                    indexArrEven++;
+                }
+                else
+                {
+                    arrOdd[indexArrOdd] = arrInt[i];
+                    indexArrOdd++;
+                }
+            }
+
+            Console.WriteLine("Chắn: " + String.Join(",", arrEven));
+            Console.WriteLine("Lẻ: " + String.Join(",", arrOdd));
         }
 
         static void Check()
