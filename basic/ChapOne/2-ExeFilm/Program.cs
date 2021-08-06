a456d8f660809ed7c8dcc937e342c8a89a6dbe08
vudinhquang <vudinhquangk53@gmail.com>
2021-08-06 10:00:34 +0900

    Số lớn nhất 01

--- basic/ChapOne/2-ExeFilm/Program.cs
+++ basic/ChapOne/2-ExeFilm/Program.cs
@@ -7,22 +7,6 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            ExeMaxNumber();
-        }
-
-        static void ExeMaxNumber()
-        {
-            int numberOne = 285;
-            int numberTwo = 125;
-            int numberThree = 185;
-            int numberFour = 325;
-
-            Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("ExeMaxNumber");
-        }
-
-        static void ExeFilm()
-        {
             // age
             // >= 16 Được xem phim Titanic
             // < 16 Không được xem phim Titanic
