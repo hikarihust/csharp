fed2dbb4754cf5d43c24e3bab1b75b8c342758ee
vudinhquang <vudinhquangk53@gmail.com>
2021-08-06 10:50:47 +0900

    BCC 02

--- csharp/csharp/Program.cs
+++ csharp/csharp/Program.cs
@@ -7,13 +7,26 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            ExeBCCOne();
+            ExeBCCTwo();
+        }
+
+        static void ExeBCCTwo()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 2; j <= 9; j++)
+                {
+                    Console.Write("{0} x {1} = {2} \t", j, i, i*j);
+                }
+                Console.WriteLine();
+            }
         }
 
         static void ExeBCCOne()
         {
             Console.OutputEncoding = Encoding.UTF8;
-            for (int i = 0; i <= 9; i++)
+            for (int i = 2; i <= 9; i++)
             {
                 for (int j = 1; j <= 10; j++)
                 {
