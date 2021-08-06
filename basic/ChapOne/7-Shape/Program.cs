9f5b2b0fa4689922a1d84915d4d91680c376c207
vudinhquang <vudinhquangk53@gmail.com>
2021-08-06 15:03:52 +0900

    Shape 02

--- basic/ChapOne/7-Shape/Program.cs
+++ basic/ChapOne/7-Shape/Program.cs
@@ -21,11 +21,15 @@ namespace csharp
             int height = 10;
             for (int i = 1; i <= height; i++)
             {
-                for (int j = 1; j <= i; j++)
-                {
-                    Console.Write("*");
-                }
+                PrintCharacterOnLine("*", i);
                 Console.WriteLine();
+            }
+        }
+        private static void PrintCharacterOnLine(string character, int totalCharacter)
+        {
+            for (int j = 1; j <= totalCharacter; j++)
+            {
+                Console.Write(character);
             }
         }
 
