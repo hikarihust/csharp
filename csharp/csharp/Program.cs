2a2af2e6feb189f879d5d65f7853782d70e0b581
vudinhquang <vudinhquangk53@gmail.com>
2021-08-08 02:05:17 +0900

    Kiểm tra chuỗi thời gian hợp lệ với Regular Expression

--- csharp/csharp/Program.cs
+++ csharp/csharp/Program.cs
@@ -11,7 +11,41 @@ namespace csharp
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            CheckID();
+            CheckDate();
+        }
+
+        static void CheckDate()
+        {
+            // Nhận chuỗi ngày sinh nhập vào từ bàn phím, In ra hợp lệ hoặc không hợp lệ. ID hợp lệ thỏa
+            // Chiều dài 8 ký tự có dạng XX-YY-ZZZZ
+            // XX ngày sinh
+            // YY tháng sinh
+            // ZZZZ là năm sinh (ZZZZ phải có giá trị từ năm 1945 - năm hiện tại) // 2021
+            // Kiểm tra xem ngày tháng năm có hợp lệ hay không  31/02/2021 ko hợp lệ
+
+            string dateInput = "30-11-2000";
+            bool flagCheck = false;
+
+            Regex regex = new Regex(@"^\d{2}-\d{2}-\d{4}$");
+            Match match = regex.Match(dateInput);
+            if (match.Success)
+            {
+                try
+                {
+                    DateTime dateTime = DateTime.ParseExact(dateInput, "dd-MM-yyyy", null);
+                    int dateInputYear = dateTime.Year;
+                    int currentYear = DateTime.Now.Year;
+
+                    if (dateInputYear < currentYear && dateInputYear > 1945) flagCheck = true;
+                }
+                catch (Exception)
+                {
+                    flagCheck = false;
+                }
+            }
+
+            if (flagCheck == true) Console.WriteLine("Hợp lệ");
+            else Console.WriteLine("KHÔNG Hợp lệ");
         }
 
         static void CheckID()
