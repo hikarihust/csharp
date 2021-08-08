1cce9255cff95526c0bfb67e02d39743ccd4de1c
vudinhquang <vudinhquangk53@gmail.com>
2021-08-08 23:51:56 +0900

    Class Book

---
+++ csharp/csharp/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp
+{
+    class Book
+    {
+        #region Thuộc tính
+        private string id;
+        private string name;
+        private int price;
+        #endregion
+
+        #region Constructor
+        public Book(string id, string name, int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return "ID: " + this.id + "\t\t Name: " + this.name
+                            + "\t\t Price: " + this.price;
+        }
+    }
+}
