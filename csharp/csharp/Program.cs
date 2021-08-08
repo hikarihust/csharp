0c3ae53c78f2826fec96e3f8389c21598181794e
vudinhquang <vudinhquangk53@gmail.com>
2021-08-09 02:08:16 +0900

    Menu

--- csharp/csharp/Program.cs
+++ csharp/csharp/Program.cs
@@ -9,11 +9,60 @@ namespace csharp
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Book bookObj = new Book("C#", 1000);
+			Console.OutputEncoding = Encoding.UTF8;
+			int functionID = 0;
+			bool flag = true;
 
-            bookObj.Price = 2000;
-            Console.WriteLine(bookObj.Name);
-            Console.WriteLine(bookObj.Price);
-        }
-    }
+			do
+			{
+				showMenu();
+				try
+				{
+					functionID = Convert.ToInt32(Console.ReadLine());
+
+					switch (functionID)
+					{
+						case 1: addBook(); break;
+						case 2: editBook(); break;
+						case 3: infoBook(); break;
+						case 4: 
+						default:
+							flag = false;
+							break;
+					}
+				}
+				catch (Exception)
+				{
+                    Console.WriteLine("Có lỗi hệ thống!");
+				}
+
+			} while (flag == true);
+		}
+
+		public static void addBook()
+		{
+            Console.WriteLine("addBook");
+		}
+
+		public static void editBook()
+		{
+            Console.WriteLine("editBook");
+		}
+
+		public static void infoBook()
+		{
+            Console.WriteLine("infoBook");
+		}
+
+		public static void showMenu()
+		{
+
+            Console.WriteLine("===================== BOOK MANAGER =====================");
+            Console.WriteLine("1. Add book");
+            Console.WriteLine("2. Edit book");
+            Console.WriteLine("3. Info book (by ID)");
+            Console.WriteLine("4. Exit");
+			Console.Write("Your choise [1-4]: ");
+		}
+	}
 }
