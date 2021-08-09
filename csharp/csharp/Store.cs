6d071b64d8692c5cc723813b146add015c4b7f27
vudinhquang <vudinhquangk53@gmail.com>
2021-08-09 23:26:21 +0900

    Store

---
+++ csharp/csharp/Store.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp
+{
+    class Store
+    {
+        #region Thuộc tính
+        private const int MAX_ITEMS = 5;
+        private List<Book> listItems = null;
+        #endregion
+
+        #region Constructor
+        public Store()
+        {
+            listItems = new List<Book>(MAX_ITEMS);
+        }
+        #endregion
+
+        public bool checkExist(string bookId)
+        {
+            return false;
+        }
+
+        public bool checkFull()
+        {
+            return false;
+        }
+
+        public bool checkEmty()
+        {
+            return false;
+        }
+
+        public void add(Book bookObj)
+        {
+
+        }
+
+        public void edit(string bookId, string bookName, int bookPrice)
+        {
+        
+        }
+
+        public void delete(string bookId)
+        {
+        
+        }
+
+        public void find(string bookId)
+        {
+         
+        }
+
+        public void list()
+        {
+        
+        }
+    }
+}
